472af01d79ab6264c1bd450c49cc632ddf7e6848
Saut Manurung <68042498+sautmanurung1@users.noreply.github.com>
2023-08-17 22:56:34 +0700

    add about constant variable

--- constant/constant.go
+++ constant/constant.go
@@ -1,3 +1,5 @@
 package constant
 
-var BASE_URL_AUTH = "https://sandbox.partner.api.bri.co.id/oauth/client_credential/accesstoken"
+var BASE_URL_AUTH = "https://sandbox.partner.api.bri.co.id"
+var AUTH_URL = "/oauth/client_credential/accesstoken"
+var AUTH_VALAS = "/v2.0/valas-info/kurs-counter"
---
+++ constant/constant_directDebit.go
@@ -0,0 +1,4 @@
+package constant
+
+var BASE_URL_DirectBRI = "https://sandbox.partner.api.bri.co.id/v1.2/directdebit"
+var Card_token = "/v1.2/directdebit/tokens"
